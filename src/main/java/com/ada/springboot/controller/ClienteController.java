cc7c96e51282ad3646354e5e51b9c15ff219fffa
Andressa Raffler <andressa.raffler@sinqia.com.br>
2022-09-28 09:05:49 -0300

    -Implementação de Querys

--- src/main/java/com/ada/springboot/controller/ClienteController.java
+++ src/main/java/com/ada/springboot/controller/ClienteController.java
@@ -5,10 +5,12 @@ import com.ada.springboot.controller.vo.ClienteVO;
 import com.ada.springboot.model.Cliente;
 import com.ada.springboot.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -20,7 +22,7 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @PostMapping("/cliente")
+    @PostMapping("/gravar-cliente")
     public String criarCliente(@RequestBody ClienteVO clienteVO){
         Cliente cliente = new Cliente();
         cliente.setNome(clienteVO.getNome());
@@ -30,22 +32,20 @@ public class ClienteController {
         return "<h1> Cliente Criado </h1>";
         }
 
-    @GetMapping("/cliente/{id}")
+    @GetMapping("/buscar-cliente-por-id/{id}")
     public ResponseEntity<ClienteDTO> buscarClientePorId(@PathVariable("id") Long id){ //vai consumir da API, classe intermediária
         Optional<Cliente> optionalCliente = this.clienteService.buscarClientePorId(id);
-        if(optionalCliente.isPresent()){
-            Cliente cliente = optionalCliente.get();
-            ClienteDTO clienteDTO = new ClienteDTO();
-            clienteDTO.setNome(cliente.getNome());
-            clienteDTO.setCpf(cliente.getCpf());
-            clienteDTO.setDataNascimento(cliente.getDataNascimento());
-            return ResponseEntity.ok(clienteDTO); //Lembrando que o DTO é para retornar os dados formatados
-        }
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return getClienteDTOResponseEntity(optionalCliente);
     }
 
-    @PutMapping("/cliente/{id}")
-    public ResponseEntity<ClienteDTO>atualizarCliente(@PathVariable("id")Long id, @RequestBody ClienteVO clienteVO){
+    @GetMapping("/buscar-cliente-por-cpf/{cpf}")
+    public ResponseEntity<ClienteDTO> buscarClientePorId(@PathVariable("cpf") String cpf){ //vai consumir da API, classe intermediária
+        Optional<Cliente> optionalCliente = this.clienteService.buscarClientePorCpf(cpf);
+        return getClienteDTOResponseEntity(optionalCliente);
+    }
+
+    @PutMapping("/atualizar-cliente-por-id/{id}")
+    public ResponseEntity<ClienteDTO>atualizarClientePorId(@PathVariable("id")Long id, @RequestBody ClienteVO clienteVO){
        Cliente cliente = this.clienteService.atualizarClientePorId(id,clienteVO);
         if(Objects.nonNull(cliente)){
             ClienteDTO clienteDTO = new ClienteDTO();
@@ -57,12 +57,29 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @DeleteMapping("cliente/{id}")
+    @PutMapping("/atualizar-cliente-por-cpf/{cpf}")
+    public ResponseEntity<ClienteDTO>atualizarClientePorCpf(@RequestBody ClienteVO clienteVO){
+        Cliente cliente = this.clienteService.atualizarCliente(clienteVO);
+        if(Objects.nonNull(cliente)){
+            ClienteDTO clienteDTO = new ClienteDTO();
+            clienteDTO.setNome(cliente.getNome());
+            clienteDTO.setCpf(cliente.getCpf());
+            clienteDTO.setDataNascimento(cliente.getDataNascimento());
+            return ResponseEntity.ok().body(clienteDTO);
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping("/deletar-cliente-por-id/{id}")
     public ResponseEntity<String>removerCliente(@PathVariable("id")Long id){
-        Optional<Cliente> optionalCliente = this.clienteService.buscarClientePorId(id);
+        return getStringResponseEntity(id);
+    }
+    @DeleteMapping("/deletar-cliente-por-cpf/{cpf}")
+    public ResponseEntity<String>removerClientePorId(@PathVariable("cpf")String cpf){
+        Optional<Cliente> optionalCliente = this.clienteService.buscarClientePorCpf(cpf);
         if(optionalCliente.isPresent()){
             Cliente cliente = optionalCliente.get();
-            this.clienteService.removerClientePorId(id);
+            this.clienteService.removerClientePorCpf(cpf);
             return ResponseEntity.ok("Cliente removido com sucesso!");
         }
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Cliente não existe!");
@@ -71,6 +88,29 @@ public class ClienteController {
     @GetMapping("/clientes")
     public ResponseEntity<List<ClienteDTO>> listarTodosClientes(){
         List<Cliente> clienteList = this.clienteService.listarTodosClientes();
+        return converterClienteEmClienteDTO(clienteList);
+    }
+
+    @GetMapping("/clientes-por-nome/{nome}")
+    public ResponseEntity<List<ClienteDTO>> listarClientesPorNome(@PathVariable ("nome")String nome){
+        List<Cliente> clienteList = this.clienteService.listarClientePorNome(nome);
+        return converterClienteEmClienteDTO(clienteList);
+    }
+
+    @GetMapping("/clientes-por-nome/{nome}/ou-data-nascimento/{data-nascimento}/")
+    public ResponseEntity<List<ClienteDTO>> listarClientePorNomeOuDataNascimento(@PathVariable ("nome")String nome,
+                                                                                 @PathVariable("data-nascimento")@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataNascimento){
+        List<Cliente> clienteList = this.clienteService.listarClientePorNomeOuDataNascimentoOrdenadoPorNome(nome, dataNascimento);
+        return converterClienteEmClienteDTO(clienteList);
+    }
+
+    @GetMapping("/clientes-nascidos-entre/{data-inicial}/e/{data-final}/")
+    public ResponseEntity<List<ClienteDTO>> listarClientesPorData( @PathVariable("data-inicial")@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicial,
+                                                                                 @PathVariable("data-final")@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFinal){
+        List<Cliente> clienteList = this.clienteService.listarClientesPorPeriodo(dataInicial, dataFinal);
+        return converterClienteEmClienteDTO(clienteList);
+    }
+    private ResponseEntity<List<ClienteDTO>> converterClienteEmClienteDTO(List<Cliente> clienteList) {
         if(clienteList.isEmpty()){
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
@@ -85,5 +125,26 @@ public class ClienteController {
         return ResponseEntity.ok(clienteDTOS);
     }
 
+    private ResponseEntity<ClienteDTO> getClienteDTOResponseEntity(Optional<Cliente> optionalCliente) {
+        if (optionalCliente.isPresent()) {
+            Cliente cliente = optionalCliente.get();
+            ClienteDTO clienteDTO = new ClienteDTO();
+            clienteDTO.setNome(cliente.getNome());
+            clienteDTO.setCpf(cliente.getCpf());
+            clienteDTO.setDataNascimento(cliente.getDataNascimento());
+            return ResponseEntity.ok(clienteDTO); //Lembrando que o DTO é para retornar os dados formatados
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    private ResponseEntity<String> getStringResponseEntity(@PathVariable("id") Long id) {
+        Optional<Cliente> optionalCliente = this.clienteService.buscarClientePorId(id);
+        if(optionalCliente.isPresent()){
+            Cliente cliente = optionalCliente.get();
+            this.clienteService.removerClientePorId(id);
+            return ResponseEntity.ok("Cliente removido com sucesso!");
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Cliente não existe!");
+    }
 
 }
--- src/main/java/com/ada/springboot/service/ClienteService.java
+++ src/main/java/com/ada/springboot/service/ClienteService.java
@@ -6,6 +6,7 @@ import com.ada.springboot.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,6 +40,21 @@ public class ClienteService {
     return null; // MELHORAR ESSE CARA
     }
 
+    public Cliente atualizarCliente(ClienteVO dadosAtualizadosCliente){
+        Optional<Cliente> optionalCliente = this.clienteRepository.findByCpf(dadosAtualizadosCliente.getCpf());
+        if(optionalCliente.isPresent()){
+            Cliente cliente = optionalCliente.get();
+            Cliente clienteAtualizado = new Cliente();
+            clienteAtualizado.setId(cliente.getId());
+            clienteAtualizado.setNome(dadosAtualizadosCliente.getNome());
+            clienteAtualizado.setCpf(dadosAtualizadosCliente.getCpf());
+            clienteAtualizado.setDataNascimento(dadosAtualizadosCliente.getDataNascimento());
+            return this.clienteRepository.save(clienteAtualizado); //devolve a própria entidade atualizada
+        }
+        //throw new RuntimeException("Cliente nao encontrado");
+        return null; // MELHORAR ESSE CARA
+    }
+
     public void removerClientePorId(Long id){
         this.clienteRepository.deleteById(id);
     }
@@ -46,4 +62,27 @@ public class ClienteService {
     public List<Cliente> listarTodosClientes(){
         return this.clienteRepository.findAll();
     }
+
+    public List<Cliente> listarClientePorNome(String nome){
+        return this.clienteRepository.findByNome(nome);
+    }
+    public List<Cliente> listarClientePorNomeOuDataNascimentoOrdenadoPorNome(String nome, LocalDate dataNascimento){
+        return this.clienteRepository.findByNomeContainingOrDataNascimentoOrderByNomeAsc(nome, dataNascimento);
+    }
+
+    public List<Cliente> listarClientesPorPeriodo(LocalDate dataInicial, LocalDate dataFinal){
+        //return this.clienteRepository.findByDataNascimentoBetween(dataInicial, dataFinal); //utilizando metodos
+        return this.clienteRepository.listarClientesPorPeriodo(dataInicial, dataFinal); //UTILIZANDO QUERYS
+
+    }
+
+    public Optional<Cliente> buscarClientePorCpf(String cpf){
+        return clienteRepository.findByCpf(cpf);
+    }
+
+    public void removerClientePorCpf(String cpf) {
+        this.clienteRepository.deleteByCpf(cpf);
+    }
+
 }
+
